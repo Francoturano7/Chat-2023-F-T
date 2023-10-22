72945bbf47f10e91947ecdc231da5838994c4792
Franco_Turano <francoturano777@gmail.com>
2023-10-22 16:34:45 -0300

    Subiendo

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import{Server} from "socket.io"
 
 import { viewsRouter } from "./routes/views.routes.js"
 
-const port= procces.env.PORT || 8080
+const port= process.env.PORT || 8080
 const app=express()
 
 //midlewares
